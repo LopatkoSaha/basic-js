2fd4fe3c124bf337b8623f84d88d69a24506c049
Lopatko Saha <lopatkoaleksandr2@gmail.com>
2023-04-14 01:27:14 +0300

    fix: some tasks

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -8,17 +8,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  * const depthCalc = new DepthCalculator();
  * depthCalc.calculateDepth([1, 2, 3, 4, 5]) => 1
- * depthCalc.calculateDepth([1, 2, 3, [4, 5]]) => 2
+ * depthCalc.calculateDepth([1, [1, 3], 3, [4, [1, 2]]) => 2
  * depthCalc.calculateDepth([[[]]]) => 3
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    return 1 + Math.max(...arr.map(element => {
+      if (Array.isArray(element)) {
+        if (element.length === 0) {
+          return 1;
+        }
+        return this.calculateDepth(element);
+      }else{
+        return 0;
+      }
+    }));
   }
 }
-
 module.exports = {
   DepthCalculator
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,11 +13,68 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!(arr instanceof Array)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+  const result = [];
+  let skipNext = false;
+  arr.forEach((item, idx) => {
+    if (skipNext) {
+      skipNext = false;
+      return;
+    }
+    switch (item) {
+      case "--discard-prev":
+        if (!result.length) {
+          return;
+        }
+        return result.splice(idx - 1, 1);
+      case "--discard-next":
+        if (idx === arr.length - 1) {
+          return;
+        }
+        return skipNext = true;
+      case "--double-prev":
+        if (!result.length) {
+          return;
+        }
+        return result.push(result.at(-1));
+      case "--double-next":
+        if (idx === arr.length - 1) {
+          return;
+        }
+        return result.push(arr[idx + 1]);
+      default: result.push(item);
+    }
+    // if (item === "--discard-next") {
+    //   result.splice(ind, 2)
+    // }
+    // if (item ==="--discard-prev" && ind === 0 || isNaN(item) && ind === 0) {
+    //   result.splice(ind, 1)
+    // }
+    // if (isNaN(item) && ind === 0) {
+    //   result.splice(ind, 1)
+    // }
+    // if (item ==="--discard-prev") {
+    //   result.splice(ind - 1, 2)
+    // }
+    // if (item === "--double-next") {
+    //   result.splice(ind, 1, ind + 1)
+    // }
+    // if (item === "--double-prev") {
+    //   result.splice(ind, 1, ind - 1)
+    // }
+  });
+  return result
 }
+
+// console.log(transform([ '--discard-prev', 1, 2, 3]));
 
 module.exports = {
   transform
 };
+// --discard-next исключает следующий элемент массива из преобразованного массива.
+// --discard-prev исключает предыдущий элемент массива из преобразованного массива.
+// --double-next дублирует следующий элемент массива в преобразованном массиве.
+// --double-prev дублирует предыдущий элемент массива в преобразованном массиве
