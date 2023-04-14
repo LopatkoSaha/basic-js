85edbb699638ec0d5e840c1ecf281c988dbf8608
Lopatko Saha <lopatkoaleksandr2@gmail.com>
2023-04-15 00:57:00 +0300

    add: solution two tasks

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,11 +15,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, {
+  repeatTimes = 1,
+  separator = "+",
+  addition = "",
+  additionRepeatTimes = 1,
+  additionSeparator = "|",
+}) {
+  const additionSubstring = new Array(additionRepeatTimes).fill(`${addition}`).join(additionSeparator);
+  return new Array(repeatTimes).fill(`${str}${additionSubstring}`).join(separator);
 }
 
 module.exports = {
   repeater
 };
+// repeatTimesустанавливает количество numberповторений str;
+// separatorявляется stringразделяющим повторением str;
+// additionявляется дополнительным string, который будет добавляться к каждому повторению str;
+// additionRepeatTimesустанавливает количество numberповторений addition;
+// additionSeparatorявляется stringразделяющим повторением addition.
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,57 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor (dir = true) {
+    this.dir = dir; 
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getTransformedKey (message = '', key = '') {
+    if (key.length >= message.length){
+      return key.toUpperCase();
+    }
+    let source = key;
+    while (source.length < message.length) {
+      source += key;
+    }
+    let result = "";
+    let keyOffset = 0;
+    const words = message.split(" ");
+    words.forEach((word) => {
+      result += source.slice(keyOffset, keyOffset + word.length) + " ";
+      keyOffset += word.length;
+    })
+    return result.toUpperCase();
+  }
+  cryptSymbol (symb = '', key = '', isCrypt) {
+    const keyCode = key.toUpperCase().charCodeAt();
+    const symbCode = symb.toUpperCase().charCodeAt();
+    if (symbCode < 65 || symbCode > 90) {
+      return symb;
+    }
+    if(isCrypt) {
+      return String.fromCharCode((keyCode + symbCode - 130)%26 + 65);
+    }
+    let diff = (symbCode - keyCode) < 0 ? (symbCode - keyCode) + 26 : (symbCode - keyCode);
+    return String.fromCharCode(diff + 65);
+  }
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error("Incorrect arguments!");
+    }
+    const transformedKey = this.getTransformedKey(message, key);
+    const result = message.split('').map((symb, idx) => {
+      return this.cryptSymbol(symb, transformedKey[idx], true);
+    }).join('');
+    return this.dir ? result : result.split("").reverse().join("");
+  }
+  decrypt(message, key) {
+    if (!message || !key) {
+      throw new Error("Incorrect arguments!");
+    }
+    const transformedKey = this.getTransformedKey(message, key);
+    const result = message.split('').map((symb, idx) => {
+      return this.cryptSymbol(symb, transformedKey[idx], false);
+    }).join('');
+    return this.dir ? result : result.split("").reverse().join("");
   }
 }
 
